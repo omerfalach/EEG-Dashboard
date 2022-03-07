c0be42d4378f7d76565fc5123e7cd0e8a8ff458b
omerfalach <54241496+omerfalach@users.noreply.github.com>
2022-03-07 17:11:45 +0200

    Update eeg_dash.py

--- eeg_dash.py
+++ eeg_dash.py
@@ -1,118 +1,83 @@
-import os
-import sys
-from os.path import join
-from matplotlib.backends.backend_agg import RendererAgg
+import os.path as op
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 import mne
-import streamlit as st
+from mne.datasets import somato
+from mne.baseline import rescale
+from mne.stats import bootstrap_confidence_interval
+data_path = somato.data_path()
+subject = '01'
+task = 'somato'
+raw_fname = op.join(data_path, 'sub-{}'.format(subject), 'meg',
+                    'sub-{}_task-{}_meg.fif'.format(subject, task))
 
-# Page Configuration 
-st.set_page_config(
-    page_title="Filterung von EEG-Daten",
-    page_icon="📈",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    )
+# let's explore some frequency bands
+iter_freqs = [
+    ('Theta', 4, 7),
+    ('Alpha', 8, 12),
+    ('Beta', 13, 25),
+    ('Gamma', 30, 45)
+]
+# set epoching parameters
+event_id, tmin, tmax = 1, -1., 3.
+baseline = None
 
-# cache is turned off for Raw-Object, maybe if we want to implement reloading after changing some features
-# of the raw-object (n_channels, sfreq), we could implement a custom hash-function for that
-@st.cache(hash_funcs={mne.io.fiff.raw.Raw: lambda _: None}, allow_output_mutation=True)
-def load_raw():
-    sample_data_folder = mne.datasets.sample.data_path()
-    sample_data_raw_file = join(sample_data_folder, 'MEG', 'sample',
-                                'sample_audvis_raw.fif')
-    raw = mne.io.read_raw_fif(sample_data_raw_file)
+# get the header to extract events
+raw = mne.io.read_raw_fif(raw_fname)
+events = mne.find_events(raw, stim_channel='STI 014')
 
-    # All the following methods operate in-Place
-    # Reducing data to time window in [s]
-    raw.crop(0, 60)
+frequency_map = list()
 
-    # Resampling takes quit a long time, maybe filtering the higher sampling-rate is faster than first resampling
-    #   and then filtering the resampled data
-
-    # # Resampling data to sampling-frequency in [Hz]
-    # st.write('Reducing SampleRate')
-    # loaded_raw.resample(200)
-
-    # Picking only Gradiometer-Channels (example for slow drift in this data)
-    raw.pick_types(meg=False, eeg=True, stim=False, eog=False)
-    # Uncomment this and comment the previous line to see some EKG-Artefacts
-    # raw.pick_types(meg='mag', eeg=False, stim=False, eog=False)
-
-    # Data has to be loaded into memory for filtering afterwards
+for band, fmin, fmax in iter_freqs:
+    # (re)load the data to save memory
+    raw = mne.io.read_raw_fif(raw_fname)
+    raw.pick_types(meg='grad', eog=True)  # we just look at gradiometers
     raw.load_data()
 
-    return raw
+    # bandpass filter
+    raw.filter(fmin, fmax, n_jobs=1,  # use more jobs to speed up.
+               l_trans_bandwidth=1,  # make sure filter params are the same
+               h_trans_bandwidth=1)  # in each band and skip "auto" option.
 
+    # epoch
+    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline=baseline,
+                        reject=dict(grad=4000e-13, eog=350e-6),
+                        preload=True)
+    # remove evoked response
+    epochs.subtract_evoked()
 
-# cache is turned off for Raw-Object, should only depend on hp/lp-parameters
-@st.cache(hash_funcs={mne.io.fiff.raw.Raw: lambda _: None}, allow_output_mutation=True)
-def filter_raw(raw, hp, lp):
-    with st.spinner(text='Filtering'):
-        raw = raw.copy().filter(hp, lp)
-    return raw
+    # get analytic signal (envelope)
+    epochs.apply_hilbert(envelope=True)
+    frequency_map.append(((band, fmin, fmax), epochs.average()))
+    del epochs
+del raw
 
+# Helper function for plotting spread
+def stat_fun(x):
+    """Return sum of squares."""
+    return np.sum(x ** 2, axis=0)
+# Plot
+fig, axes = plt.subplots(4, 1, figsize=(10, 7), sharex=True, sharey=True)
+colors = plt.get_cmap('winter_r')(np.linspace(0, 1, 4))
+for ((freq_name, fmin, fmax), average), color, ax in zip(
+        frequency_map, colors, axes.ravel()[::-1]):
+    times = average.times * 1e3
+    gfp = np.sum(average.data ** 2, axis=0)
+    gfp = mne.baseline.rescale(gfp, times, baseline=(None, 0))
+    ci_low, ci_up = bootstrap_confidence_interval(average.data, random_state=0,
+                                                  stat_fun=stat_fun)
+    ci_low = rescale(ci_low, average.times, baseline=(None, 0))
+    ci_up = rescale(ci_up, average.times, baseline=(None, 0))
+        ax.fill_between(times, gfp + ci_up, gfp - ci_low, color=color, alpha=0.3)
+    ax.grid(True)
+    ax.set_ylabel('GFP')
+    ax.annotate('%s (%d-%dHz)' % (freq_name, fmin, fmax),
+                xy=(0.95, 0.8),
+                horizontalalignment='right',
+                xycoords='axes fraction')
+    ax.set_xlim(-1000, 3000)
 
-@st.cache(allow_output_mutation=True)
-def FigureCache():
-    return {'EEG-Plot': dict(),
-            'PSD-Plot': dict()}
-
-# Hide Burger Menu
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-st.title('EEG-Filter Demo')
-st.sidebar.write('<Erklär-Text>')
-
-loaded_raw = load_raw()
-# Get Filter-Parameters
-highpass = st.sidebar.slider('Hochpass-Filter', min_value=0, max_value=100, value=0)
-lowpass = st.sidebar.slider('Tiefpass-Filter', min_value=0, max_value=100, value=100)
-
-# Filter raw
-raw_filtered = filter_raw(loaded_raw, highpass, lowpass)
-
-figure_cache = FigureCache()
-# Create a string as hash from the Filter-Parameters
-filter_hash = f'{highpass}-{lowpass}'
-
-# Lock functionality used to fix a bug with Matplot which influences multiuser interaction.
-_lock = RendererAgg.lock
-
-# Loading cached figure or creating a new one
-if filter_hash in figure_cache['EEG-Plot']:
-    # Just for debugging
-    st.write('Loading cached figure')
-    filtered_image = figure_cache['EEG-Plot'][filter_hash]
-
-
-st.write('EEG-Daten gefiltert:')
-st.image(filtered_image)
-
-# Loading cached figure or creating a new one
-if filter_hash in figure_cache['PSD-Plot']:
-    # Just for debugging
-    st.write('Loading cached figure')
-    psd_image = figure_cache['PSD-Plot'][filter_hash]
-else:
-    with _lock:
-        # Just for debugging
-        st.write('Producing new figure')
-        psd_fig = raw_filtered.plot_psd(show=False)
-        psd_fig.canvas.draw()
-        psd_image = np.fromstring(psd_fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-        psd_image = psd_image.reshape(psd_fig.canvas.get_width_height()[::-1] + (3,))
-        figure_cache['PSD-Plot'][filter_hash] = psd_image
-
-st.write('Frequenzspektrum:')
-st.image(psd_image)
-
-cache_size = sum([sum([sys.getsizeof(figure_cache[plot_type][freq_hash]) for freq_hash in figure_cache[plot_type]])
-                  for plot_type in figure_cache])
-st.write(f'Figure-Cache takes {cache_size} bytes')
+axes.ravel()[-1].set_xlabel('Time [ms]')
+st.pyplot(fig)
