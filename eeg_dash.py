297f1fc96b1ac680546af7712670b8e786348d58
omerfalach <54241496+omerfalach@users.noreply.github.com>
2022-03-07 17:42:35 +0200

    Update eeg_dash.py

--- eeg_dash.py
+++ eeg_dash.py
@@ -6,7 +6,21 @@ import matplotlib.pyplot as plt
 from mne.datasets import sample
 from mne import read_evokeds
 
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
 
+
+def space(num_lines=1):
+    """Adds empty lines to the Streamlit app."""
+    for _ in range(num_lines):
+        st.write("")
+
+
+st.set_page_config(layout="centered", page_icon="💬", page_title="Physician View")
+
+# Data visualisation part
+
+st.title("💬 Physician View"")
 
 path = sample.data_path()
 fname = path + '/MEG/sample/sample_audvis-ave.fif'
@@ -22,7 +36,15 @@ times = np.arange(0.05, 0.151, 0.02)
 all_times = np.arange(-0.2, 0.5, 0.03)
 
 extrapolations = ['local', 'head', 'box']
-times = np.arange(0.05, 0.151, 0.01)
-fig, anim = evoked.animate_topomap(
-    times=times, ch_type='mag', frame_rate=2, time_unit='s', blit=False)
+fig, axes = plt.subplots(figsize=(7.5, 4.5), nrows=2, ncols=3)
+
+# Here we look at EEG channels, and use a custom head sphere to get all the
+# sensors to be well within the drawn head surface
+for axes_row, ch_type in zip(axes, ('mag', 'eeg')):
+    for ax, extr in zip(axes_row, extrapolations):
+        evoked.plot_topomap(0.1, ch_type=ch_type, size=2, extrapolate=extr,
+                            axes=ax, show=False, colorbar=False,
+                            sphere=(0., 0., 0., 0.09))
+        ax.set_title('%s %s' % (ch_type.upper(), extr), fontsize=14)
+fig.tight_layout()
 st.pyplot(fig)
